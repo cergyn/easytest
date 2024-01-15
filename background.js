8ad2ec5bac5e555885dc0119c246840d4c8e822a
Celal Ergun <cergyn@malwarebytes.com>
2024-01-15 13:55:47 +0200

    Add filter logic

--- background.js
+++ background.js
@@ -48,9 +48,49 @@ function blockUrls(easyListRules) {
 const blockedUrls = easyList.split("\n").filter((rule) => rule.startsWith("||")).map((rule) => rule.slice(2));
 
 // This event is fired when the extension is first installed.
-chrome.runtime.onInstalled.addListener(function() {
-  console.log('The extension has been installed!');
-});
+const separatorCharacters = ':?/=^'
+function indexOfFilter (input, filter, startingPos) {
+  if (filter.length > input.length) {
+    return -1
+  }
+
+  const filterParts = filter.split('^')
+  let index = startingPos
+  let beginIndex = -1
+  let prefixedSeparatorChar = false
+
+  for (let f = 0; f < filterParts.length; f++) {
+    if (filterParts[f] === '') {
+      prefixedSeparatorChar = true
+      continue
+    }
+
+    index = input.indexOf(filterParts[f], index)
+    if (index === -1) {
+      return -1
+    }
+    if (beginIndex === -1) {
+      beginIndex = index
+    }
+
+    if (prefixedSeparatorChar) {
+      if (separatorCharacters.indexOf(input[index - 1]) === -1) {
+        return -1
+      }
+    }
+    // If we are in an in between filterPart
+    if (f + 1 < filterParts.length &&
+            // and we have some chars left in the input past the last filter match
+            input.length > index + filterParts[f].length) {
+      if (separatorCharacters.indexOf(input[index + filterParts[f].length]) === -1) {
+        return -1
+      }
+    }
+
+    prefixedSeparatorChar = false
+  }
+  return beginIndex
+}
 
 // This event is fired before a request is sent.
 chrome.webRequest.onBeforeRequest.addListener(
